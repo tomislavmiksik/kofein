47a00706fdf3e0c07af49eeb36fbe5329c9935ba
Tomislav Miksik <tomislav.miksik@gmail.com>
2026-04-15 15:14:10 +0200

    fix: manifest fix for PWA

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,8 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'manifest', href: '/manifest.webmanifest' },
   ],
   htmlAttrs: { lang: 'en' }
 })
